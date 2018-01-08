ef4f38aad0a7d52df8c87820c785f5f127507a58
Didier 'OdyX' Raboud <odyx@liip.ch>
2018-01-08 12:37:38 +0100

    requests: Implement stage0

--- requests_gpgauthlib/gpgauth_session.py
+++ requests_gpgauthlib/gpgauth_session.py
@@ -22,11 +22,9 @@ import re
 from gnupg import GPG
 from requests import Session
 from tempfile import TemporaryDirectory
+from uuid import uuid4
 
-from .exceptions import GPGAuthException
-
-# This is passbolt_api's version
-GPGAUTH_SUPPORTED_VERSION = '1.3.0'
+from .exceptions import GPGAuthException, GPGAuthStage0Exception
 
 logger = logging.getLogger(__name__)
 
@@ -35,6 +33,9 @@ class GPGAuthSession(Session):
     """GPGAuth extension to :class:`requests.Session`.
     """
     VERIFY_URI = '/verify.json'
+
+    # This is passbolt_api's version
+    GPGAUTH_SUPPORTED_VERSION = '1.3.0'
 
     def __init__(self, auth_url, server_fingerprint, amnesic_gpg=False, **kwargs):
         """Construct a new GPGAuth client session.
@@ -49,9 +50,25 @@ class GPGAuthSession(Session):
         super(GPGAuthSession, self).__init__(**kwargs)
 
     @property
+    def _nonce0(self):
+        try:
+            return self.__nonce0
+        except AttributeError:
+            pass
+        # This format is stolen from
+        # https://github.com/passbolt/passbolt_cli/blob/master/app/models/gpgAuthToken.js
+        self.__nonce0 = 'gpgauthv%s|36|' % self.GPGAUTH_SUPPORTED_VERSION
+        self.__nonce0 += str(uuid4())
+        self.__nonce0 += '|gpgauthv%s' % self.GPGAUTH_SUPPORTED_VERSION
+        return self.__nonce0
+
+    @property
     def workdir(self):
-        if hasattr(self, '_workdir'):
+        try:
             return self._workdir
+        except AttributeError:
+            pass
+
         # Setup our home
         _userhome = os.environ.get('HOME')
         if not _userhome:
@@ -69,8 +86,10 @@ class GPGAuthSession(Session):
 
     @property
     def gpg(self):
-        if hasattr(self, '_gpg'):
+        try:
             return self._gpg
+        except AttributeError:
+            pass
 
         # Instantiate GnuPG in a specific directory
         _gpghomedirname = os.path.join(self.workdir, '.gnupg')
@@ -84,16 +103,19 @@ class GPGAuthSession(Session):
 
     @property
     def gpgauth_version_is_supported(self):
-        if hasattr(self, '_gpgauth_version_is_supported'):
+        try:
             return self._gpgauth_version_is_supported is True
+        except AttributeError:
+            pass
 
+        # We don't know, let's verify
         r = self.head(self.auth_url)
         if 'X-GPGAuth-Version' not in r.headers:
             logger.debug(r.headers)
             raise GPGAuthException(
                 "GPGAuth support not announced by %s" % self.auth_url
             )
-        if r.headers['X-GPGAuth-Version'] != GPGAUTH_SUPPORTED_VERSION:
+        if r.headers['X-GPGAuth-Version'] != self.GPGAUTH_SUPPORTED_VERSION:
             raise GPGAuthException(
                 "GPGAuth Version not supported (%s != %s)" % (
                     r.headers['X-GPGAuth-Version'],
@@ -105,9 +127,9 @@ class GPGAuthSession(Session):
         return True
 
     @property
-    def server_key(self):
+    def server_fingerprint(self):
         if hasattr(self, '_server_key'):
-            return self._server_key
+            return self._server_fingerprint
 
         if not self.gpgauth_version_is_supported:
             return False
@@ -116,7 +138,7 @@ class GPGAuthSession(Session):
         server_key = self.gpg.export_keys([self._server_fingerprint], secret=False, subkeys=False)
         if 'BEGIN PGP PUBLIC KEY BLOCK' in server_key:
             self._server_key = server_key
-            return self._server_key
+            return self._server_fingerprint
 
         # Try to get it from the server
         r = self.get(self.auth_url + self.VERIFY_URI)
@@ -135,5 +157,97 @@ class GPGAuthSession(Session):
         logger.info('server_fingerprint(): 0x%s '
                     'imported successfully' % self._server_fingerprint)
         self._server_key = _server_key
+        return self._server_fingerprint
 
-        return self._server_key
+    @property
+    def user_fingerprint(self):
+        try:
+            return self._user_fingerprint
+        except AttributeError:
+            pass
+
+        # Try to get them from GPG
+        secret_keys = self.gpg.list_keys(secret=True)
+        if not secret_keys:
+            raise GPGAuthException('No user fingerprint was loaded, you need to call import_user_private_key_from_file()')
+        # Assume the main key is the first
+        self._user_fingerprint = secret_keys.fingerprints[0]
+        return self._user_fingerprint
+
+    def import_user_private_key_from_file(self, user_private_key_file):
+        # Import the user private key
+        with open(user_private_key_file, 'r') as key:
+            logger.info('Importing the user private key; password prompt expected')
+            import_result = self.gpg.import_keys(key.read())
+            if len(import_result.fingerprints) < 1:
+                raise GPGAuthException('No key could be imported')
+            else:
+                [
+                    logger.info('GPG key 0x%s successfully imported' % key)
+                    for key in import_result.fingerprints
+                ]
+                self._user_fingerprint = import_result.fingerprints.pop()
+        return self._user_fingerprint
+
+    def server_identity_verified(self):
+        """ GPGAuth stage0 """
+        try:
+            return self._server_identity_verified
+        except AttributeError:
+            pass
+
+        # Encrypt a uuid token for the server
+        server_verify_token = self.gpg.encrypt(self._nonce0,
+                                               self.server_fingerprint)
+        if not server_verify_token.ok:
+            raise GPGAuthStage0Exception(
+                'Encryption of the nonce0 (%s) '
+                'to the server fingerprint (%s) failed.' %
+                (self._nonce0, self.server_fingerprint)
+            )
+
+        r = self.post(
+            self.auth_url + self.VERIFY_URI,
+            json={'gpg_auth': {
+                'keyid': self.user_fingerprint,
+                'server_verify_token': str(server_verify_token)
+                }
+            },
+        )
+
+        validation_errors = []
+        if r.headers['X-GPGAuth-Authenticated'] != 'false':
+            validation_errors.append(
+                GPGAuthStage0Exception(
+                    'X-GPGAuth-Authenticated should be set to false'))
+        if r.headers['X-GPGAuth-Progress'] != 'stage0':
+            validation_errors.append(
+                GPGAuthStage0Exception(
+                    'X-GPGAuth-Progress should be set to stage0'))
+        if 'X-GPGAuth-User-Auth-Token' in r.headers:
+            validation_errors.append(
+                GPGAuthStage0Exception(
+                    'X-GPGAuth-User-Auth-Token should not be set'))
+        if 'X-GPGAuth-Verify-Response' not in r.headers:
+            validation_errors.append(
+                GPGAuthStage0Exception(
+                    'X-GPGAuth-Verify-Response should be set'))
+        if 'X-GPGAuth-Refer' in r.headers:
+            validation_errors.append(
+                GPGAuthStage0Exception(
+                    'X-GPGAuth-Refer should not be set'))
+
+        if validation_errors:
+            logger.warning(r.headers)
+            raise validation_errors.pop()
+
+        if r.headers['X-GPGAuth-Verify-Response'] != self._nonce0:
+            raise GPGAuthStage0Exception(
+                'The server decrypted something different than what we sent '
+                '(%s <> %s)' %
+                (r.headers['X-GPGAuth-Verify-Response'], self._nonce0))
+        self._server_identity_verified = True
+        logger.info('server_identity_verified(): OK')
+
+    # GPGAuth stages in numerical form
+    stage0 = server_identity_verified
