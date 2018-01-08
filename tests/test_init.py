df90ac2158e543f16d8dc85474fcad83e1c54130
Didier 'OdyX' Raboud <odyx@liip.ch>
2018-01-08 14:01:37 +0100

    Fix tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,9 +1,9 @@
 import pytest
 
-from gpgauth import GPGAuth
+from requests_gpgauthlib.gpgauth_session import GPGAuthSession
 
 
 def test_init_void():
     # No Arguments, it fails
     with pytest.raises(TypeError):
-        GPGAuth()
+        GPGAuthSession()
