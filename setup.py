017711dfff6cc74cc4cb78ee05dec5e38564987e
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-07-26 08:42:34 +0200

    Update version number to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name='requests_gpgauthlib',
-      version='0.1.1',
+      version='0.1.2',
       description='requests_gpgauthlib - A requests GPGAuth authentication library',
       author='Didier \'OdyX\' Raboud',
       author_email='odyx@liip.ch',
